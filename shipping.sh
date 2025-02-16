8d734059825a70dfb66882d7d6b6278e2e275d08
santoshpawar02 <santosh.pawar02@gmail.com>
2025-02-16 17:54:05 +0530

    shipping err

--- shipping.sh
+++ shipping.sh
@@ -6,7 +6,8 @@ print_head Install mysql and SQL files
 dnf install mysql -y  &>>$log_file 
 exit_status_print $?
 
-for file in schema app-user master-date; do 
+
+for file in schema app-user master-data; do 
     print_head Load $file
     mysql -h mysql-dev.santoshpawar.site -uroot -pRoboShop@1 < /app/db/$file.sql  &>>$log_file
 done
